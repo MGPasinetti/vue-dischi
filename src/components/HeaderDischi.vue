decf46121ba1b441f84ecdd908d673b44033f12e
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-24 15:45:27 +0100

    starting asset and bootstrap installation

---
+++ src/components/HeaderDischi.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    sono l'HEADER
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: `HeaderDischi`,
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainDischi.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    sono il MAIN
+  </main>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: `MainDischi`,
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
